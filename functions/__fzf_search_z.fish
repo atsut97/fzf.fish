6f4cadeb87b0ed85ee3819c0361dd1cb4b9d714d
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2020-12-16 16:53:02 +0900

    Return immediately if unable to find z

--- functions/__fzf_search_z.fish
+++ functions/__fzf_search_z.fish
@@ -1,4 +1,9 @@
 function __fzf_search_z --description "Search the list of most used directories stored by z. Insert the selected path into the commandline at the cursor."
+    if not type -q z
+        echo '__fzf_search_z: Unable to find z(1) in the system' >&2
+        return 1
+    end
+
     # Make sure that fzf uses fish to execute __fzf_preview_file.
     # See similar comment in __fzf_search_shell_variables.fish.
     set --local --export SHELL (command --search fish)
