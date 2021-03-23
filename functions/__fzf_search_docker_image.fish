a53b27d3ef78714bb3dc6491e6323a665390bb95
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-03-23 20:21:10 +0900

    Exit with error when the docker daemon is not running

--- functions/__fzf_search_docker_image.fish
+++ functions/__fzf_search_docker_image.fish
@@ -2,6 +2,9 @@ function __fzf_search_docker_image --description "Search the docker top-level im
     if not type -q docker
         echo '__fzf_search_docker_image: Unable to find docker(1) on the system' >&2
         return 1
+    else if not docker stats --no-stream >/dev/null 2>&1
+        echo '__fzf_search_docker_container: Cannot connect to the Docker daemon' >&2
+        return 1
     end
 
     # Make sure that fzf uses fish to execute __fzf_preview_file.
