2d94471b14dadc5f57a62880f0451a3da24ef984
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-04-23 00:13:17 +0900

    Add test for __fzf_search_git_branch

---
+++ tests/search_git_branch/outputs_right_branch.fish
@@ -0,0 +1,6 @@
+mock commandline "--current-token --replace --" "echo \$argv"
+mock commandline \* ""
+set --export --append FZF_DEFAULT_OPTS "--filter='main'"
+set expected main
+set actual (__fzf_search_git_branch)
+@test "outputs right branch name" "$actual" = "$expected"
