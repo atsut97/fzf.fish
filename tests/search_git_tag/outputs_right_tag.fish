df6442d8f9103c35be2ecfb0a5f4904744b4fe49
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-04-24 18:28:10 +0900

    Add mock of 'git tag' to avoid CI error

--- tests/search_git_tag/outputs_right_tag.fish
+++ tests/search_git_tag/outputs_right_tag.fish
@@ -1,5 +1,8 @@
 mock commandline "--current-token --replace --" "echo \$argv"
 mock commandline \* ""
+# Emulates the command 'git tag' to print a specific tag since tags
+# are not downloaded on the CI environment.
+mock git tag "echo v6.2"
 set --export --append FZF_DEFAULT_OPTS "--filter='v6.2'"
 set expected 'v6.2'
 set actual (__fzf_search_git_tag)
