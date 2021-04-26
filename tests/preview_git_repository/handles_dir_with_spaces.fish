b1a12e75fa40832f11edefa15928008c232dc78d
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-04-26 13:52:16 +0900

    Add tests to validate handling of paths with spaces

---
+++ tests/preview_git_repository/handles_dir_with_spaces.fish
@@ -0,0 +1,16 @@
+# Make a temporary repository
+set --local repo 'tests/_resources/repo with spaces'
+rm -rf "$repo"
+mkdir -p "$repo"
+git init --quiet "$repo"
+
+# Remove ANSI color codes
+mock bat "--style=numbers --color=always" "cat \$argv"
+
+set contents "# Spaced Repository"
+echo "$contents" >"$repo/README.md"
+set actual (__fzf_preview_git_repository "$repo")
+
+@test "correctly handles directory paths with spaces" "$actual" = "$contents"
+
+rm -rf "$repo"
---
+++ tests/search_z/handles_directories_with_spaces.fish
@@ -0,0 +1,14 @@
+mock commandline "--current-token --replace --" "echo \$argv"
+mock commandline \* ""
+mock z --list "\
+echo '50         /home/atsuta/multi word directory';
+echo '16         /home/atsuta/directory 1';
+echo '1.25       /home/atsuta/directory 2';
+"
+mock z \* "return 0"
+set --export --append FZF_DEFAULT_OPTS "--filter='directory'"
+
+set actual (__fzf_search_z)
+set expected "'/home/atsuta/directory 1' '/home/atsuta/directory 2' '/home/atsuta/multi word directory'"
+
+@test "handles directories with spaces correctly" "$actual" = $expected
