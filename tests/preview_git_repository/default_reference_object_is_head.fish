9f2129d615c1621009cf100e141cd3fec4cb5af1
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2021-06-02 11:42:57 +0900

    Improve test robustness

--- tests/preview_git_repository/default_reference_object_is_head.fish
+++ tests/preview_git_repository/default_reference_object_is_head.fish
@@ -1,6 +1,5 @@
-set line (__fzf_preview_git_repository . log | head -n 1 | string split " ")
-# Remove ANSI color codes
-set actual (echo "$line[3]" | cat -v | sed 's/\^[[[0-9;]*m//g')
+set line (__fzf_preview_git_repository . log | head -n 1)
+set actual (echo "$line" | cat -v | string match --regex '[0-9a-f]{7}')
 set expected (git rev-parse --short HEAD)
 
 @test "uses HEAD object to preview git repository by default" "$actual" = "$expected"
